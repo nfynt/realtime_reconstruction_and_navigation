b8e82116a09a4272a068edc238d455844d7d36b2
Shubham Singh <shubham.nfynt@gmail.com>
2020-01-22 08:58:03 +0000

    camera calib

--- OpenCVLib/OpenCVDriver/CameraClib.cpp
+++ OpenCVLib/OpenCVDriver/CameraClib.cpp
@@ -2,20 +2,93 @@
 
 #include "CameraClib.h"
 
-using namespace std;
-using namespace cv;
-namespace fs = std::filesystem;
-
-void CameraClib::DetermineCameraParams(string imagePath)
+bool CameraClib::InitCaliberation(int horizontalCorners, int verticalCorners, int frameWidth, int frameHeight)
 {
-	for (auto &p : fs::directory_iterator(imagePath))
+	numBoards = 0;
+	numCornersHor = horizontalCorners;
+	numCornersVer = verticalCorners;
+	numSquares = numCornersHor * numCornersVer;
+
+	board_sz = Size(numCornersHor, numCornersVer);
+	frame_sz = Size(frameWidth, frameHeight);
+
+	object_points.clear();
+	image_points.clear();
+	corners.clear();
+
+	intrinsic = Mat(3, 3, CV_32FC1);
+
+	intrinsic.ptr<float>(0)[0] = 1;
+	intrinsic.ptr<float>(1)[1] = 1;
+
+	finishedCalberation = false;
+
+	return true;
+}
+
+bool CameraClib::Caliberate(Mat image, int* indOut)
+{
+	vector<Point3f> obj;
+	Mat grayImage;
+	cvtColor(image, grayImage, COLOR_BGR2GRAY);
+
+	for (int j = 0; j < numSquares; j++)
+		obj.push_back(Point3f(j / numCornersHor, j%numCornersHor, 0.0f));
+
+	bool found = findChessboardCorners(image, board_sz, corners, CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_FILTER_QUADS);
+
+	if (found)
 	{
-		cout << p.path() << endl;
+		cornerSubPix(grayImage, corners, Size(11, 11), Size(-1, -1), TermCriteria(TermCriteria::EPS | TermCriteria::MAX_ITER, 30, 0.1));
+		//drawChessboardCorners(grayImage, board_sz, corners, found);
+		image_points.push_back(corners);
+		object_points.push_back(obj);
+
+		//printf("Snap stored!");
+
 	}
+	framesProcessed++;
+
+	return found;
+}
+
+bool CameraClib::FinishCaliberation(bool saveParamsToFile, string path)
+{
+	calibrateCamera(object_points, image_points, frame_sz, intrinsic, distCoeffs, rvecs, tvecs);
+	finishedCalberation = true;
+}
+
+Mat CameraClib::GetIntrinsicMatrix()
+{
+	if (finishedCalberation) return intrinsic;
+	else NULL;
+}
+
+Mat CameraClib::GetDistortionCoefficients()
+{
+	if (finishedCalberation) return distCoeffs;
+	else NULL;
+}
+
+vector<Mat> CameraClib::GetRotationVectors()
+{
+	if (finishedCalberation) return rvecs;
+	else NULL;
+}
+vector<Mat> CameraClib::GetTranslationVectors()
+{
+	if (finishedCalberation) return tvecs;
+	else NULL;
+}
+
+Mat CameraClib::LoadCameraParamFromFile(string path)
+{
+
 }
 
 CameraClib::CameraClib()
 {
+	//this->InitCaliberation(6, 9);
 }
 
 
--- OpenCVLib/OpenCVDriver/CameraClib.h
+++ OpenCVLib/OpenCVDriver/CameraClib.h
@@ -2,14 +2,56 @@
 
 #include <iostream>
 #include <string>
-#include <filesystem>
+#include <vector>
 #include "opencv2/opencv.hpp"
+
+using namespace std;
+using namespace cv;
 
 class CameraClib
 {
+private:
+	int numBoards;
+	int numCornersHor;
+	int numCornersVer;
+	int numSquares;
+
+	cv::Size board_sz;
+	cv::Size frame_sz;
+
+	vector<vector<Point3f>> object_points;
+	vector<vector<Point2f>> image_points;
+
+	vector<Point2f> corners;
+	int successes = 0;
+
+	cv::Mat intrinsic;
+	cv::Mat distCoeffs;
+	vector<Mat> rvecs;
+	vector<Mat> tvecs;
+
+	int framesProcessed;
+	bool finishedCalberation;
+
 public:
 	CameraClib();
 	~CameraClib();
-	void DetermineCameraParams(std::string imagePath);
+	//Reset current params for new caliberation
+	bool InitCaliberation(int horizontalCorners, int verticalCorners, int frameWidth, int frameHeight);
+	
+	//Caliberate current calculation with camera image. indOut set's the total number of frames processed
+	bool Caliberate(cv::Mat img, int* indOut=NULL);
+	
+	//Close caliberation and save to params to local file
+	bool FinishCaliberation(bool saveParamsToFile, std::string path=NULL);
+	
+	//Get intrinsic matrix (3,3,CV_32FC1)
+	cv::Mat GetIntrinsicMatrix();
+	cv::Mat GetDistortionCoefficients();
+	std::vector<cv::Mat> GetRotationVectors();
+	std::vector<cv::Mat> GetTranslationVectors();
+	
+	//Load camera param from local file and initialise camera param
+	cv::Mat LoadCameraParamFromFile(std::string path);
 };
 
